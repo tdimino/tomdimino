2d9027a92574662b6e98ec8e0c9984185b09da68
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-19 18:39:51 -0400

    Update artifex.js

--- artifex.js
+++ artifex.js
@@ -1,23 +1,32 @@
-// Listen for my Kaph and click
-kaphtorObjects.forEach(function(object) {
-    object.addEventListener('load', function() {
-        var svgDoc = object.contentDocument; // Get the SVG document inside the Object tag
-        var svgRoot = svgDoc.documentElement; // Get the root SVG element
+document.addEventListener('DOMContentLoaded', function() {
+    var desiredTitles = ["nūn", "mēm", "dālet", "mēm", "tāw"];
 
-        // Attach the click event listener to the SVG root element
-        svgRoot.addEventListener('click', function() {
-            setTimeout(function() {
-                if (isDesiredTitle(object.title)) {
-                    // Flash like a cock, then spin
-                    svgRoot.classList.add('flashColor', 'rotate');
+    function isDesiredTitle(title) {
+        return desiredTitles.includes(title);
+    }
 
-                    // Omit thy spark and cry until thy play is done
-                    setTimeout(function() {
-                        svgRoot.classList.remove('flashColor', 'rotate');
-                    }, 2000); // Reign of Kronos in the space between 
-                }
-            }, 500); // An aeon's repose 
+    var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
+
+    // Listen for my Kaph and click
+    kaphtorObjects.forEach(function(object) {
+        object.addEventListener('load', function() {
+            var svgDoc = object.contentDocument; // Get the SVG document inside the Object tag
+            var svgRoot = svgDoc.documentElement; // Get the root SVG element
+
+            // Attach the click event listener to the SVG root element
+            svgRoot.addEventListener('click', function() {
+                setTimeout(function() {
+                    if (isDesiredTitle(object.title)) {
+                        // Flash like a cock, then spin
+                        svgRoot.classList.add('flashColor', 'rotate');
+
+                        // Omit thy spark and cry until thy play is done
+                        setTimeout(function() {
+                            svgRoot.classList.remove('flashColor', 'rotate');
+                        }, 2000); // Reign of Kronos in the space between 
+                    }
+                }, 500); // An aeon's repose 
+            });
         });
     });
 });
-
