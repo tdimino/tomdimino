f19b5be41e10aea1804d4513269d1161215cb9ce
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-19 19:09:59 -0400

    Update artifex.js

--- artifex.js
+++ artifex.js
@@ -5,28 +5,23 @@ document.addEventListener('DOMContentLoaded', function() {
         return desiredTitles.includes(title);
     }
 
-    var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
+    var kaphtorSVGs = document.querySelectorAll('.Kaphtor svg');
 
     // Listen for my Kaph and click
-    kaphtorObjects.forEach(function(object) {
-        object.addEventListener('load', function() {
-            var svgDoc = object.contentDocument; // Get the SVG document inside the Object tag
-            var svgRoot = svgDoc.documentElement; // Get the root SVG element
+    kaphtorSVGs.forEach(function(svg) {
+        // Attach the click event listener to the SVG root element
+        svg.addEventListener('click', function() {
+            setTimeout(function() {
+                if (isDesiredTitle(svg.id)) {
+                    // Flash like a cock, then spin
+                    svg.classList.add('flashColor', 'rotate');
 
-            // Attach the click event listener to the SVG root element
-            svgRoot.addEventListener('click', function() {
-                setTimeout(function() {
-                    if (isDesiredTitle(object.title)) {
-                        // Flash like a cock, then spin
-                        svgRoot.classList.add('flashColor', 'rotate');
-
-                        // Omit thy spark and cry until thy play is done
-                        setTimeout(function() {
-                            svgRoot.classList.remove('flashColor', 'rotate');
-                        }, 2000); // Reign of Kronos in the space between 
-                    }
-                }, 500); // An aeon's repose 
-            });
+                    // Omit thy spark and cry until thy play is done
+                    setTimeout(function() {
+                        svg.classList.remove('flashColor', 'rotate');
+                    }, 2000); // Reign of Kronos in the space between 
+                }
+            }, 500); // An aeon's repose 
         });
     });
 });
