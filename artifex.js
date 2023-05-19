3ff9428d7e25abf29cd603edde42ef671003e2cf
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-19 15:20:30 -0400

    Update artifex.js
    Artifex strikes back and shames the listless bard.

--- artifex.js
+++ artifex.js
@@ -1,25 +1,25 @@
-//
-var desiredTitles = ["nūn", "mēm", "dālet", "mēm", "tāw"];
+document.addEventListener('DOMContentLoaded', function() {
+    var desiredTitles = ["nūn", "mēm", "dālet", "mēm", "tāw"];
 
-function isDesiredTitle(title) {
-    return desiredTitles.includes(title);
-}
+    function isDesiredTitle(title) {
+        return desiredTitles.includes(title);
+    }
 
-var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
-
-// Listen for my Kaph and click
-kaphtorObjects.forEach(function(object) {
-    object.addEventListener('click', function() {
-        setTimeout(function() {
-            if (isDesiredTitle(object.title)) {
-        // Flash like a cock, then spin
-                object.classList.add('flashColor', 'rotate');
-                
-        // Omit thy spark and cry until thy play is done
-                setTimeout(function() {
-                    object.classList.remove('flashColor', 'rotate');
-                }, 2000); // Reign of Kronos in the space between 
-            }
-        }, 500); // An aeon's repose 
+    // Listen for my Kaph and click
+    var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
+    kaphtorObjects.forEach(function(object) {
+        object.addEventListener('click', function() {
+            setTimeout(function() { // An aeon's repose 
+                if (isDesiredTitle(object.title)) {
+                    // Flash like a cock, then spin
+                    object.classList.add('flashColor', 'rotate');
+                    
+                    // Omit thy spark and cry until thy play is done
+                    setTimeout(function() {
+                        object.classList.remove('flashColor', 'rotate');
+                    }, 2000); // Reign of Kronos in the space between 
+                }
+            }, 500); 
+        });
     });
 });
