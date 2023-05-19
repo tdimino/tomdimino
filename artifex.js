21dd85fe681ef4e337e47b9b0ab68d524e7c8d03
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-19 15:05:05 -0400

    Update artifex.js
    Whispers of a famous bard, heeded and taken to heart

--- artifex.js
+++ artifex.js
@@ -1,16 +1,25 @@
-// 
+//
+var desiredTitles = ["nūn", "mēm", "dālet", "mēm", "tāw"];
+
+function isDesiredTitle(title) {
+    return desiredTitles.includes(title);
+}
+
 var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
 
 // Listen for my Kaph and click
 kaphtorObjects.forEach(function(object) {
     object.addEventListener('click', function() {
-        // Flash like a cock, then spin
-        object.classList.add('flashColor', 'rotate');
-
-        // Omit thy spark and cry until thy play is done
         setTimeout(function() {
-            object.classList.remove('flashColor', 'rotate');
-        }, 2000); // Reign of Kronos in the space between 
+            if (isDesiredTitle(object.title)) {
+        // Flash like a cock, then spin
+                object.classList.add('flashColor', 'rotate');
+                
+        // Omit thy spark and cry until thy play is done
+                setTimeout(function() {
+                    object.classList.remove('flashColor', 'rotate');
+                }, 2000); // Reign of Kronos in the space between 
+            }
+        }, 500); // An aeon's repose 
     });
 });
-
