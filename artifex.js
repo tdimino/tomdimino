c8b4580d26fa436b124d5c0a906682f2b9d71c0a
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-19 18:29:02 -0400

    Update artifex.js

--- artifex.js
+++ artifex.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var desiredTitles = ["nūn", "mēm", "dālet", "mēm", "tāw"];
+// Listen for my Kaph and click
+kaphtorObjects.forEach(function(object) {
+    object.addEventListener('load', function() {
+        var svgDoc = object.contentDocument; // Get the SVG document inside the Object tag
+        var svgRoot = svgDoc.documentElement; // Get the root SVG element
 
-    function isDesiredTitle(title) {
-        return desiredTitles.includes(title);
-    }
-
-    // Listen for my Kaph and click
-    var kaphtorObjects = document.querySelectorAll('.Kaphtor object');
-    kaphtorObjects.forEach(function(object) {
-        object.addEventListener('click', function() {
-            setTimeout(function() { 
+        // Attach the click event listener to the SVG root element
+        svgRoot.addEventListener('click', function() {
+            setTimeout(function() {
                 if (isDesiredTitle(object.title)) {
                     // Flash like a cock, then spin
-                    object.classList.add('flashColor', 'rotate');
-                    
+                    svgRoot.classList.add('flashColor', 'rotate');
+
                     // Omit thy spark and cry until thy play is done
                     setTimeout(function() {
-                        object.classList.remove('flashColor', 'rotate');
+                        svgRoot.classList.remove('flashColor', 'rotate');
                     }, 2000); // Reign of Kronos in the space between 
                 }
             }, 500); // An aeon's repose 
         });
     });
 });
+
