146b6f1663b6be006bcc5e54f0072c402fad21cd
Tom di Mino <93949958+tdimino@users.noreply.github.com>
2023-05-20 13:08:18 -0400

    Update artifex.js

--- artifex.js
+++ artifex.js
@@ -1,39 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // Define an array of desired symbols
-    const desiredSymbols = ['nūn', 'mēm', 'dālet', 'mēm', 'tāw'];
+  // Define an array of desired symbols
+  const desiredSymbols = ['nūn', 'mēm', 'dālet', 'tāw'];
 
-    // Define a function to rotate an SVG element around its Y axis
-    function rotateY(svg, degrees, duration) {
-        return gsap.to(svg, {
-            rotationY: degrees,
-            duration: duration,
-            perspective: 1000,
-            scale3D: 1, 1, 1
+  // Get all of the SVG elements with the class "symbol"
+  const symbolSVGs = document.querySelectorAll('.symbol');
+
+  // Iterate over the SVG elements and add a click event listener to each one
+  symbolSVGs.forEach(function(svg) {
+
+    // Attach the click event listener to the SVG root element
+    svg.addEventListener('click', function() {
+
+      // Check if the ID of the clicked element is in the desired symbols array
+      if (desiredSymbols.includes(svg.id)) {
+
+        // Rotate the element around its Y axis for 2 seconds using GSAP
+        gsap.to(svg, {
+          rotationY: '+=360',
+          duration: 2,
+          perspective: 1000,
+          scale3D: [1, 1, 1]
         });
-    }
-
-    // Get all of the SVG elements with the class "Kaphtor"
-    const kaphtorSVGs = document.querySelectorAll('.Kaphtor svg');
-
-    // Iterate over the SVG elements and add a click event listener to each one
-    kaphtorSVGs.forEach(function(svg) {
-
-        // Attach the click event listener to the SVG root element
-        svg.addEventListener('click', function() {
-
-            // Check if the ID of the clicked element is in the desired symbols array
-            if (desiredSymbols.includes(svg.id)) {
-
-                // Rotate the element around its Y axis for 2 seconds
-                const animationId = requestAnimationFrame(function() {
-                    rotateY(svg, 360, 2);
-                    requestAnimationFrame(this);
-                });
-
-                // After 2 seconds, stop rotating the element
-                cancelAnimationFrame(animationId);
-            }
-        });
+      }
     });
+  });
 });
